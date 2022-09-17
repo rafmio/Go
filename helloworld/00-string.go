7c2f6483e3c774d4c26e9a288a98a00df09032d3
rafael idrisov <raf@raf.raf>
2022-09-17 20:45:43 +0300

    Nobody else

---
+++ helloworld/00-string.go
@@ -0,0 +1,11 @@
+String
+In Go strings are different from other languages like Java, C++, Python, etc. 
+It is a sequence of variable-width character where each and evry character is represented by one or more bytes using UTF-8 Encoding.
+
+Strings are immutable chain of arbitrary bytes (including bytes with zero value) or strings are a read-only slice of bytes and bytes of the strings can be represented in the Unicode text using UTF-8 encoding.
+
+In Go, strings are immutable once a string is created the value of the string cannot be changed. String are read-only. If you try to change it, then the compiler will throw an error. 
+
+You can iterate over string using for rang loop. This loop can iterate over the Unicode code point for a string. 
+
+// https://www.geeksforgeeks.org/strings-in-golang/
---
+++ helloworld/string3.go
@@ -0,0 +1,4 @@
+
+
+// Trim a String
+// https://www.geeksforgeeks.org/how-to-trim-a-string-in-golang/

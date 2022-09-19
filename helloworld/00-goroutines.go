679d4375692b73cdab92543fdc8c6ac67e1a5100
rafael idrisov <raf@raf.raf>
2022-09-19 20:48:26 +0300

    Hola

---
+++ helloworld/00-goroutines.go
@@ -0,0 +1,6 @@
+Goroutines
+Goroutines is a function or method which executes imdependently and simultaneously in connection with any other Goroutines present in your program. 
+Every concurrently executing activity in Go is known as a Goroutines.
+You can consider a Goroutines like a ligth weighted thread. 
+
+// https://www.geeksforgeeks.org/goroutines-concurrency-in-golang/

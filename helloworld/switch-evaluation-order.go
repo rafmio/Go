f834e2fe7690bbe987c172e1f5365fb11fc24709
rafael idrisov <raf@raf.raf>
2022-09-20 21:27:08 +0300

    Comeetee

--- helloworld/switch-evaluation-order.go
+++ helloworld/switch-evaluation-order.go
@@ -1,35 +1,24 @@
 package main
-
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 func main() {
-	fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away.")
-	}
-	
-	fmt.Println("When's Friday?")
-    switch time.Friday {
+    fmt.Println("When's Saturday?")
+    today := time.Now().Weekday()
+    
+    switch time.Saturday {
         case today + 0:
             fmt.Println("Today")
         case today + 1:
+            fmt.Println("Tomorrow")
+        case today + 2:
             fmt.Println("In two days")
         default:
             fmt.Println("Too far away")
     }
-    
-    fmt.Println("Value of 'today' variable is: %g", today)
-    whattime := time.Now()
-    fmt.Println("Time is: %g", whattime)
 }
+
+// Switch evaluation order
+// https://go.dev/tour/flowcontrol/10
--- helloworld/switch-with-no-condition.go
+++ helloworld/switch-with-no-condition.go
@@ -1,11 +1,24 @@
 package main
-import (
-  "fmt"
-  "time"
+import  (
+    "fmt"
+    "time"
 )
 
 func main() {
-  t := time.Now()
+    t := time.Now()
+    fmt.Printf("time.Now(): %s\n", t)
+    
+    switch {
+        case t.Hour() < 12:
+            fmt.Println("Good morning!")
+        case t.Hour() < 17:
+            fmt.Println("Good afternoon")
+        default:
+            fmt.Println("Good evening")
+    }
+    
+    
+    fmt.Printf("h.Hour(): %d\n", t.Hour())
 }
 
 // Switch with no condition

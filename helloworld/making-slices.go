13309efc5b487f39a20f40d67c1ae4e51540f936
rafael idrisov <raf@raf.raf>
2022-09-21 22:38:53 +0300

    file-mile

--- helloworld/making-slices.go
+++ helloworld/making-slices.go
@@ -1,41 +1,31 @@
 package main
 import "fmt"
 
-func main() {
-    a := make([]int, 5)
-    printSlice("a", a)
-    
-    for i := 0; i < cap(a); i++ {
-        a[i] = (i + 1) * i
-    }
-    printSlice("a", a)
-    
-    b := make([]int, 10, 10)
-    printSlice("b", b)
-    
-    for i := 0; i < len(b); i++ {
-        b[i] = (i + 1) * i
-    }
-    printSlice("b", b)
-    
-    c := b[:2]
-    printSlice("c", c)
-    
-    d := c[2:5]
-    printSlice("d", d)
-}
-
-func printSlice(s string, x []int) {
-    fmt.Printf("%s len = %d cap = %d %v\n", 
+func printSlice(s string, x []int) { 
+    fmt.Printf("%s len = %d, cap = %d %v\n", 
                s, len(x), cap(x), x)
 }
 
-// Slices can be created with the built-in 'make' function;
-// this is how you create dynamically-sized arrays
-// The 'make' funcions allocates a zeroed array and returns 
-// a slice that refers to that array
+func main() {
+    aaa := make([]int, 5)
+    aaa[4] = 1000
+    printSlice("aaa: ", aaa)
+    
+    bbb := make([]int, 1, 5)
+    bbb[0] = 2000
+    printSlice("bbb: ", bbb)
+    
+    ccc := bbb[:5]
+    printSlice("ccc: ", ccc)
+    
+    ccc[4] = 1_500
+    printSlice("ccc: ", ccc)
+    
+    ddd := ccc[2:5]
+    printSlice("ddd: ", ddd)
+   
+}
 
-// To specify a capacity, pass a third argument to 'make':
-// b := make([]int, 0, 5)   - len(b)=0, cap(b)=5
-// b = b[:cap(b)]           - len(b)=5, cap(b)=5
-// b = b[1:]                - len(b)=5, cap(b)=4
+
+// Creating a slice with make
+// https://go.dev/tour/moretypes/13

8dcefd51aaf7df6785a19a62af53fa4e2329a7aa
rafael idrisov <raf@raf.raf>
2022-09-22 20:52:05 +0300

    return it

--- helloworld/function-values.go
+++ helloworld/function-values.go
@@ -1,8 +1,17 @@
 package main
+
 import (
     "fmt"
     "math"
 )
+
+func sum(aaa, bbb int) int {
+    return aaa + bbb
+}
+
+func mult(fn func(int, int) int) int {
+    return fn(10, 20)
+}
 
 func compute(fn func(float64, float64) float64) float64 {
     return fn(3, 4)
@@ -10,13 +19,19 @@ func compute(fn func(float64, float64) float64) float64 {
 
 func main() {
     hypot := func(x, y float64) float64 {
-        return math.Sqrt(x * x + y * y)
+        return math.Sqrt(x * x + y* y)
     }
+    
     fmt.Println(hypot(5, 12))
     fmt.Println(compute(hypot))
     fmt.Println(compute(math.Pow))
+    
+    aaa := 100
+    bbb := 200
+    fmt.Println("sum(aaa, bbb): ", sum(aaa, bbb))
+    fmt.Println("mult(sum):     ", mult(sum))
+    
 }
-
 
 // Function values
 // Functions are values too. They can be passed around just like

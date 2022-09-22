deb01265af408b57c529a326f715297da09a2c61
rafmio <rafmio@yandex.ru>
2022-09-22 09:20:11 +0300

    HWKMTB

--- helloworld/range.go
+++ helloworld/range.go
@@ -1,17 +1,23 @@
 package main
 import "fmt"
 
-var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128, 512}
+var msg = []string{"Huggy", "Wuggy", "Kissy", "Missy", "Toko", "Boko"}
 
 func main() {
-    for i, v := range pow {
-        fmt.Printf("2 ** %d = %d\n", i, v)
-    }
+  for i, v := range pow {
+    fmt.Printf("2 ** %d = %d\n", i, v)
+  }
+
+  for indx, inmsg := range msg {
+    fmt.Printf("index: %d, message: %s\n", indx, inmsg)
+  }
+
 }
 
-// Range
-// The 'range' from of the 'for' loop iterals over a slice or map
-// When ranging over a slice, two values are returned for each
-// iteration. The first is the indexx, and the second is a copy of
-// the element at that index 
 
+// Range
+// https://go.dev/tour/moretypes/16
+// Return value:
+// First: index
+// Second: copy of the element at that index

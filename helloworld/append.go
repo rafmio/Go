3283b072087ff7a317b6ed77556bbeab68ae095d
rafmio <rafmio@yandex.ru>
2022-09-22 09:10:15 +0300

    Who is your daddy?

--- helloworld/append.go
+++ helloworld/append.go
@@ -2,34 +2,26 @@ package main
 import "fmt"
 
 func main() {
-    var s []int
-    printSlice(s)
-    
-    // Append works on nil slices
-    s = append(s, 0)
-    printSlice(s)
-    
-    // The slice grows as needed
-    s = append(s, 1, 58, 36)
-    printSlice(s)
-    
-    // We can add more than one element at a time
-    s = append(s, 12, 343, 453, 500)
-    printSlice(s)
+  var s []int
+  printSlice(s)
+
+  s = append(s, 1)
+  printSlice(s)
+
+  s = append(s, 2, 3, 4, 100)
+  printSlice(s)
+
+  var sls []int
+  printSlice(sls)
+
+  sls = append(s, 200, 300, 400, 600)
+  printSlice(s)
+  printSlice(sls)
 }
 
 func printSlice(s []int) {
-    fmt.Printf("len=%d cat=%d %v\n", len(s), cap(s), s)
-    fmt.Printf("Type of s[] is: %T\n\n", s)
+  fmt.Printf("len = %d, cap = %d %v \n", len(s), cap(s), s)
 }
 
-// func append
+// Append to a slice
 // https://go.dev/tour/moretypes/15
-// func append(slice []Type, elems ...Type) []Type
-// https://pkg.go.dev/builtin#append
-// The resulting value of append is a slice containing all the
-// elements of the original slice plus the provided values
-
-// If the backing array of 's' is to small to fit all the given
-// values a bigger array will be allocated. The returned slice 
-// will point to the newly allocated array

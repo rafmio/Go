31672bfd62194b142269a6ebc8271e680dd80058
rafael idrisov <raf@raf.raf>
2022-09-24 15:44:29 +0300

    poi-rece

--- helloworld/methods-continued.go
+++ helloworld/methods-continued.go
@@ -1,10 +1,19 @@
 package main
+
 import (
     "fmt"
     "math"
 )
 
 type MyFloat float64
+
+type MyStruct struct {
+    aaa, bbb, ccc int
+}
+
+func (sss MyStruct) calc() int { 
+    return sss.aaa + sss.bbb + sss.ccc
+}
 
 func (f MyFloat) Abs() float64 {
     if f < 0 {
@@ -16,7 +25,12 @@ func (f MyFloat) Abs() float64 {
 func main() {
     f := MyFloat(-math.Sqrt2)
     fmt.Println(f.Abs())
+    fmt.Println("math.Sqrt2: ", math.Sqrt2)
+
+    sss := MyStruct{ 100, 200, 300 }
+    fmt.Printf("%d + %d + %d = %d\n", sss.aaa, sss.bbb, sss.ccc, sss.calc())
 }
+
 
 // Methods continued
 // You can declare a method on non-struct types, too
@@ -24,3 +38,5 @@ func main() {
 // difined in the same package as the method. You cannot declare
 // a method with a receiver whose type is defined in another package
 // (which includes the build-in types such as int)
+
+// https://go.dev/tour/methods/3

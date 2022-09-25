127c04e2209bdc56d13e9cb3a7910f838233bb45
rafael idrisov <raf@raf.raf>
2022-09-25 13:59:45 +0300

    met poi

---
+++ helloworld/methods-with-pointer-receivers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type Vertex struct {
+    X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+    v.X = v.X * f
+    v.Y = v.Y * f
+}
+
+func (v *Vertex) Abs() float64 {
+    return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+func main() {
+    v := &Vertex{3, 4}
+    fmt.Printf("Before scaling: %+v, Abs: %v\n", v, v.Abs()) 
+    
+    v.Scale(100)
+    fmt.Printf("After scaling: %+v, Abs: %v\n", v, v.Abs())
+    
+    fmt.Printf("Type of v is: %T\n", v)
+}
+
+
+// Choosing a value of pointer receiver
+// https://go.dev/tour/methods/8

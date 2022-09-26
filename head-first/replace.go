03e6fbc1a3074360ddc66d734193e07d305bb044
rafael idrisov <raf@raf.raf>
2022-09-26 20:51:03 +0300

    way

---
+++ head-first/replace.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    broken   := "G# r#cks!"
+    replacer := strings.NewReplacer("#", "o")
+    fixed    := replacer.Replace(broken)
+    
+    fmt.Printf("type of broken: %T, %v\n", broken, broken)
+    fmt.Printf("type of replace: %T, %v\n", replacer, replacer)
+    fmt.Println(fixed)
+}
+
+// Functions are belong to packages, but methods are
+// belong to concrete value

6aac0bf159d898c23781cbfdcadeb003b524b00f
rafmio <rafmio@yandex.ru>
2022-10-14 11:23:26 +0300

    Ole-ole-ole

--- src/head-first/arrays4.go
+++ src/head-first/arrays4.go
@@ -3,19 +3,26 @@ package main
 import "fmt"
 
 func main() {
-  notes := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
-  index := 1
-  fmt.Println("index: ", index, notes[index])
-  index = 3
-  fmt.Println("index: ", index, notes[index])
+	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
+	index := 1
+	fmt.Println("index: ", index, notes[index])
+	index = 3
+	fmt.Println("index: ", index, notes[index])
 
-  for i := 0; i < len(notes); i++ {
-    fmt.Println(i, notes[i])
-  }
+	for i := 0; i < len(notes); i++ {
+		fmt.Println(i, notes[i])
+	}
 
-  fmt.Println()
-  fmt.Println("for..range:")
-  for index, note := range notes {
-    fmt.Println(index, note)
-  }
+	fmt.Println()
+	fmt.Println("for..range:")
+	for index, note := range notes {
+		fmt.Println(index, note)
+	}
+
+	fmt.Println()
+	fmt.Println("using _ underscore")
+
+	for _, note := range notes {
+		fmt.Println(note)
+	}
 }

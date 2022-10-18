400769fa28fd614e821cc57383076b6d186d2265
rafmio <rafmio@yandex.ru>
2022-10-18 16:43:59 +0300

    I'll be pro

---
+++ src/head-first/average/average3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func average(numbers ...float64) float64 {
+  var sum float64 = 0
+  for _, number := range numbers {
+    sum += number
+  }
+  return sum / float64(len(numbers))
+}
+
+func main() {
+  fmt.Println(average(100, 50))
+  fmt.Println(average(90.7, 89.7, 98.5, 92.3))
+  fmt.Println(average(100, 150, 200, 250))
+}

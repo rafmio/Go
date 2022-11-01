ea5b6eaf685d3d03b15b19d7d1a06d39fa6e568c
rafmio <rafmio@yandex.ru>
2022-11-01 16:46:34 +0300

    Asus 01.11.2022 16:46

---
+++ src/head-first/goroutines/goroutines1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "log"
+  "net/http"
+)
+
+func main() {
+  response, err := http.Get("https://example.com")
+  fmt.Printf("Type of response: %T\n", response)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer response.Body.Close()
+  body, err := ioutil.ReadAll(response.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(string(body))
+}

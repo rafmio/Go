fbac864de067a483068fd73eb0e8b02238a2a36b
rafmio <rafmio@yandex.ru>
2022-11-02 18:46:14 +0300

    Asus 02.11.2022 18:46

---
+++ src/head-first/testing/01-main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+  "prose"
+)
+
+func main() {
+  phrases := []string{"my parents", "a rodeo clown"}
+  fmt.Println("A photo of", prose.JoinWithCommas(phrases))
+  phrases = []string{"my parents", "a rodeo clown", "a prize bull"}
+  fmt.Println("A photo of", prose.JoinWithCommas(phrases))
+  phrases = append(phrases, "Kissi-Missy")
+  fmt.Println("A photo of", prose.JoinWithCommas(phrases))
+}

938e43994dfa31104fe768c98e882e51cfdf7ceb
rafael idrisov <raf@raf.raf>
2022-11-13 11:39:36 +0300

    HP 13.11.2022 11:39

--- src/pro-go/partyinvites/main.go
+++ src/pro-go/partyinvites/main.go
@@ -34,11 +34,33 @@ func listHandler(writer http.ResponseWriter, request *http.Request) {
   templates["list"].Execute(writer, responses)
 }
 
+type formData struct {
+    *Rsvp
+    Errors []string
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+    if request.Method == http.MethodGet {
+        templates["form"].Execute(writer, formData {
+            Rsvp: &Rsvp{}, Errors: []string{},
+        })
+    } else if request.Method == http.MethodPost {
+        request.ParseForm()
+        responseData := Rsvp {
+            Name: request.Form["name"][0],
+            Email: request.Form["email"][0],
+            Phone: request.Form["phone"][0],
+            WillAttend: request.Form["willattend"][0] == "true",
+        }
+    }
+}
+
 func main() {
     loadTemplates()
 
     http.HandleFunc("/", welcomeHandler)
     http.HandleFunc("/list", listHandler)
+    http.HandleFunc("/form", formHandler)
 
     err := http.ListenAndServe(":5000", nil)
     if (err != nil) {

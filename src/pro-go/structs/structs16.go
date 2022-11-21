3abe971f1ed80e0467b7a5f91a2d97d93e304603
rafmio <rafmio@yandex.ru>
2022-11-21 19:58:41 +0300

    Asus 21.11.2022 19:58

---
+++ src/pro-go/structs/structs16.go
@@ -0,0 +1,34 @@
+// Undertanding pointers to values
+package main
+
+import "fmt"
+
+type Product struct {
+  name, category string
+  price float64
+}
+
+func calcTax(product *Product) {
+  if (product.price > 100) {
+    product.price += product.price * 0.2
+  }
+}
+
+func main() {
+  kayak := &Product {
+    name: "Kayak",
+    category: "Watersports",
+    price: 275.00,
+  }
+
+  fmt.Printf("Type of kayak: %T\n", kayak)
+  calcTax(kayak)
+
+  fmt.Println("Name:", kayak.name, "Category:", kayak.category,
+  "Price:", kayak.price)
+
+}
+
+// There is no need to assing a struct value to a variable before creating
+// a pointer, and the address operator cab used directly with the literal
+// struct syntax

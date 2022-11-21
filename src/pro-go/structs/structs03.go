56465428f47de561764e057842b9b863a1a234a5
rafmio <rafmio@yandex.ru>
2022-11-21 15:23:51 +0300

    Asus 21.11.2022 15:23

---
+++ src/pro-go/structs/structs03.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+  type Product struct {
+    name, category string
+    price float64
+  }
+
+  kayak := Product {
+    name: "Kayak",
+    category: "Waterspoerts",
+  }
+
+  fmt.Println(kayak.name, kayak.category, kayak.price)
+  kayak.price = 300
+  fmt.Println("Changed price:", kayak.price)
+
+  var lifejacket Product
+  fmt.Println("Name is zero value:", lifejacket.name == "")
+  fmt.Println("Category is zero value:", lifejacket.category == "")
+  fmt.Println("Price is zero value:", lifejacket.price == 0)
+}
+
+// You may see conde that usese the built-in new function to create struct values:
+// var lifejacket = new(Product)
+// The result is a pointer to a struct value whose fields are initialized with
+// their type's zero value. This is equivalent to this statenent:
+// var lifejacket = &Product{}
---
+++ src/pro-go/structs/structs04.go
@@ -0,0 +1,17 @@
+// Using field positions to create struct values
+package main
+
+import "fmt"
+
+func main() {
+  type Product struct {
+    name, category string
+    price float64
+  }
+
+  var kayak = Product{ "Kayak", "Watersports", 275.00 }
+
+  fmt.Println("Name:", kayak.name)
+  fmt.Println("Category:", kayak.category)
+  fmt.Println("Price:", kayak.price)
+}

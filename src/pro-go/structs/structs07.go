739385cb3bbb0483f667cd8c28b1301af1b695c8
rafmio <rafmio@yandex.ru>
2022-11-21 15:49:01 +0300

    Asus 21.11.2022 15:48

---
+++ src/pro-go/structs/structs07.go
@@ -0,0 +1,18 @@
+// Comparing struct values
+package main
+
+import "fmt"
+
+func main() {
+  type Product struct {
+    name, category string
+    price float64
+  }
+
+  p1 := Product { name: "Kayak", category: "Watersports", price: 275.00 }
+  p2 := Product { name: "Kayak", category: "Watersports", price: 275.00 }
+  p3 := Product { name: "Kayak", category: "Boats", price: 275.00 }
+
+  fmt.Println("p1 == p2:", p1 == p2)
+  fmt.Println("p1 == p2:", p1 == p3)
+}
---
+++ src/pro-go/structs/structs08.go
@@ -0,0 +1,22 @@
+// Converting between struct types
+package main
+
+import "fmt"
+
+func main() {
+  type Product struct {
+    name, category string
+    price float64
+  }
+
+  type Item struct {
+    name string
+    category string
+    price float64
+  }
+
+  prod := Product { name: "Kayak", category: "Watersports", price: 275.00 }
+  item := Item { name: "Kayak", category: "Watersports", price: 275.00 }
+
+  fmt.Println("prod == item:", prod == Product(item))
+}

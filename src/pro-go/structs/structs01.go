7967cbf81e7e9433eadfee6801657b170f50a579
rafmio <rafmio@yandex.ru>
2022-11-21 14:57:30 +0300

    Asus 21.11.2022 14:57

---
+++ src/pro-go/structs/structs01.go
@@ -0,0 +1,21 @@
+// Defining and using a struct
+package main
+
+import "fmt"
+
+func main() {
+  type Product struct {
+    name, category string
+    price float64
+  }
+
+  kayak := Product {
+    name: "Kayak",
+    category: "Waterports",
+    price: 275,
+  }
+
+  fmt.Println(kayak.name, kayak.category, kayak.price)
+  kayak.price = 300
+  fmt.Println("Changed price:", kayak.price)
+}
---
+++ src/pro-go/structs/structs02.go
@@ -0,0 +1,20 @@
+// Partially assigning struct values
+package main
+
+import "fmt"
+
+func main() {
+  type Product struct {
+    name, category string
+    price float64
+  }
+
+  kayak := Product {
+    name: "Kayak",
+    category: "Watersports",
+  }
+
+  fmt.Println(kayak.name, kayak.category, kayak.price)
+  kayak.price = 300
+  fmt.Println("Changed price:", kayak.price)
+}

6d9322bbaf653e0a2fa36a5835dd36e98ff004ff
rafmio <rafmio@yandex.ru>
2022-11-23 11:14:45 +0300

    Work 23.11.2022 11:14

---
+++ src/pro-go/methodsAndInterfaces/methods07.go
@@ -0,0 +1,38 @@
+// Defining methods for type aliases
+package main
+
+import "fmt"
+
+type Product struct {
+  name, category string
+  price float64
+}
+
+// Код работает как со звездочкой *, так и без нее
+type ProductList []Product
+
+func (products *ProductList) calcCategoryTotals() map[string]float64 {
+  totals := make(map[string]float64)
+  for _, p := range *products {
+    totals[p.category] = totals[p.category] + p.price
+  }
+  return totals
+}
+
+func main() {
+  products := ProductList {
+    { "Kayak", "Watersports", 275.00 },
+    { "Lifejacket", "Watersports", 48.95 },
+    {"Soccer Ball", "Soccer", 19.50 },
+    {"Whistle", "Soccer", 1.30 },
+  }
+
+  for category, total := range products.calcCategoryTotals() {
+    fmt.Println("Category:", category, "Total:", total)
+  }
+
+  fmt.Println()
+  returnedMap := products.calcCategoryTotals()
+  fmt.Println(returnedMap)
+  fmt.Printf("Type if returnedMap: %T\n", returnedMap)
+}
---
+++ src/pro-go/methodsAndInterfaces/methods08.go
@@ -0,0 +1,55 @@
+// Performing a type conversion
+package main
+
+import "fmt"
+
+type Product struct {
+  name, category string
+  price float64
+}
+
+type ProductList []Product
+
+func (products *ProductList) calcCategoryTotals() map[string]float64 {
+  totals := make(map[string]float64)
+  for _, p := range *products {
+    totals[p.category] = totals[p.category] + p.price
+  }
+  return totals
+}
+
+func getProducts() []Product {
+  return []Product {
+    { "Kayak", "Watersports", 275.00 },
+    { "Lifejacket", "Watersports", 48.95 },
+    { "Soccer Ball", "Soccer", 19.50 },
+  }
+}
+
+func main() {
+  products := ProductList(getProducts()) // conversion
+
+  for category, total := range products.calcCategoryTotals() {
+    fmt.Println("Category:", category, "Total:", total)
+  }
+  fmt.Println()
+  fmt.Println("----------------------------------------------")
+
+  prodTest := getProducts() // without conversion
+  for _, prod := range products {
+    fmt.Println(prod)
+  }
+  fmt.Printf("Type of products: %T\n", products)
+  fmt.Printf("Type of prodTest: %T\n", prodTest)
+  fmt.Println()
+
+  // will be an arror: 
+  // prodTest.calcCategoryTotals()
+
+
+}
+
+
+// You won't always be able to receive data with the type requiered to invoke
+// a method defined for an alias. In these situation you can perform a type
+// conversion

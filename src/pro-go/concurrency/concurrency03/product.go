7366818658dabe50fa60e0bc0df28fce14ba984f
rafmio <rafmio@yandex.ru>
2022-12-01 00:16:13 +0300

    Asus 01.12.2022 00:16

---
+++ src/pro-go/concurrency/concurrency03/product.go
@@ -0,0 +1,42 @@
+package main
+
+import "strconv"
+
+type Product struct {
+  Name string
+  Category string
+  Price float64
+}
+
+var ProductList = []*Product {
+  {"Kayak", "Watersports", 279.00},
+  {"Lifejacket", "Watersports", 49.50},
+  {"Soccer Ball", "Soccer", 19.50},
+  {"Corner Flag", "Soccer", 34.95},
+  {"Stadium", "Soccer", 79_500},
+  {"Thinking Cap", "Chess", 16},
+  {"Unsteady Chear", "Chess", 75},
+  {"Bling-bling King", "Chess", 1_200},
+  {"Submarine", "Warship Games", 1000_000},
+  {"Mine", "Warship Games", 1_000},
+  {"Torpedo", "Warship Games", 1_700},
+}
+
+type ProductGroup []*Product
+type ProductData = map[string]ProductGroup
+
+var Products = make(ProductData)
+
+func ToCurrency(val float64) string {
+  return "$ " + strconv.FormatFloat(val, 'f', 2, 64)
+}
+
+func init() {
+  for _, p := range ProductList {
+    if _, ok := Products[p.Category]; ok {
+      Products[p.Category] = append(Products[p.Category], p)
+    } else {
+      Products[p.Category] = ProductGroup{p}
+    }
+  }
+}

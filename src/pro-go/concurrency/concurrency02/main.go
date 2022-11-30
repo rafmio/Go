25127fd4068e1b62a5e0af95f1fa749c49414ad5
rafmio <rafmio@yandex.ru>
2022-11-30 16:06:01 +0300

    Asus 30.11.2022 16:05

--- src/pro-go/concurrency/concurrency02/main.go
+++ src/pro-go/concurrency/concurrency02/main.go
@@ -15,4 +15,15 @@ func main() {
   CalcStoreTotal(Products)
   // time.Sleep(time.Second * 4)
   fmt.Println("main function complete")
+
+  fmt.Println("------------------------------------------------")
+  fmt.Println()
+
+  dispatchChannel := make(chan DispatchNotification, 100)
+  go DispatchOrders(dispatchChannel)
+  for {
+    details := <- dispatchChannel
+    fmt.Println("Dispatch to", details.Customer, ":", details.Quantity,
+      "x", details.Product.Name)
+  }
 }
--- src/pro-go/concurrency/concurrency02/orderdispatch.go
+++ src/pro-go/concurrency/concurrency02/orderdispatch.go
@@ -5,3 +5,25 @@ import (
   "math/rand"
   "time"
 )
+
+type DispatchNotification struct {
+  Customer string
+  *Product
+  Quantity int
+}
+
+var Customers = []string{"Alice", "Bob", "Charlie", "Dora"}
+
+func DispatchOrders(channel chan DispatchNotification) {
+  rand.Seed(time.Now().UTC().UnixNano())
+  orderCount := rand.Intn(3) + 2
+  fmt.Println("Order count:", orderCount)
+
+  for i := 0; i < orderCount; i++ {
+    channel <- DispatchNotification {
+      Customer : Customers[rand.Intn(len(Customers) - 1)],
+      Quantity : rand.Intn(10),
+      Product  : ProductList[rand.Intn(len(ProductList) - 1)],
+    }
+  }
+}

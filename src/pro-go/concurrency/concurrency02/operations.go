935361d3db3c2ee8bfca0b0ad3061d3e49a2fe39
rafmio <rafmio@yandex.ru>
2022-11-30 09:28:46 +0300

    Work 30.11.2022 09:28

--- src/pro-go/concurrency/concurrency02/operations.go
+++ src/pro-go/concurrency/concurrency02/operations.go
@@ -5,23 +5,21 @@ import (
   "time"
 )
 
-// Parameter is a map of slices of *Product type
 func CalcStoreTotal(data ProductData) {
   var storeTotal float64
 
   fmt.Println(data)
   fmt.Printf("Type of data is: %T\n", data)
-  fmt.Println( data["Chess"])
+  fmt.Println("data['Chess']:", data["Chess"])
   fmt.Println()
 
   for category, group := range data {
     // storeTotal += group.TotalPrice(category)
-    go group.TotalPrice(category) // Total: $0.00
+    go group.TotalPrice(category)
   }
   fmt.Println("Total:", ToCurrency(storeTotal))
 }
 
-// Parameter is a slice of *Product
 func (group ProductGroup) TotalPrice(category string, ) (total float64) {
   for _, p := range group {
     fmt.Println(category, "product:", p.Name, "Price:", p.Price)
@@ -29,8 +27,6 @@ func (group ProductGroup) TotalPrice(category string, ) (total float64) {
     time.Sleep(time.Millisecond * 100)
   }
   fmt.Println(category, "subtotal:", ToCurrency(total))
-  return
+
+  return 
 }
-
-
-// Methods can be defined only on types that are created in the same package

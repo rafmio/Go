3f3296b0ae7fd02e288660d3b313081817d6d3d1
rafmio <rafmio@yandex.ru>
2022-12-05 10:16:42 +0300

    Work 05.12.2022 10:16

--- src/pro-go/unsingstrings/usingstrings04/main.go
+++ src/pro-go/unsingstrings/usingstrings04/main.go
@@ -12,7 +12,7 @@ func main() {
   var price float64
 
   fmt.Print("Enter text to scan: ")
-  n, err := fmt.Scan(&name, &category, &price)
+  n, err := fmt.Scanln(&name, &category, &price)
 
   if (err == nil) {
     Printfln("Scanned %v values", n)
@@ -20,15 +20,5 @@ func main() {
   } else {
     Printfln("Error: %v", err.Error())
   }
-  fmt.Println()
-
-  vals := make([]string, 3)
-  ivals := make([]interface{}, 3)
-  for i := 0; i < len(vals); i++ {
-    ivals[i] = &vals[i]
-  }
-  fmt.Print("Enter text to scan (3 elements): ")
-  fmt.Scan(ivals...)
-  Printfln("Name: %v", vals)
 
 }

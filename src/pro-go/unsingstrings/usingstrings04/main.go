f322883513568589312a14ba3d3606b68eabd211
rafmio <rafmio@yandex.ru>
2022-12-05 10:46:08 +0300

    Work 05.12.2022 10:46

--- src/pro-go/unsingstrings/usingstrings04/main.go
+++ src/pro-go/unsingstrings/usingstrings04/main.go
@@ -20,5 +20,32 @@ func main() {
   } else {
     Printfln("Error: %v", err.Error())
   }
+  fmt.Println()
+
+  var name2, category2 string
+  var price2 float64
+  source := "Lifejacket Watersports 48.95"
+  n, err = fmt.Sscan(source, &name2, &category2, &price2)
+  if (err == nil) {
+    Printfln("Scanned %v values", n)
+    Printfln("Name: %v, Category: %v, Price: %.2f", name2, category2, price2)
+  } else {
+    Printfln("Error: %v", err.Error())
+  }
+  fmt.Println()
+
+  var name3, category3 string
+  var price3 float64
+
+  source3 := "Product Lifejacket Watersports 48.95"
+  template3 := "Product %s %s %f"
+  n, err = fmt.Sscanf(source3, template3, &name3, &category3, &price3)
+
+  if (err == nil) {
+    Printfln("Scanned %v values", n)
+    Printfln("Name: %v, Category: %v, Price: %.2f", name3, category3, price3)
+  } else {
+    Printfln("Error: %v", err.Error())
+  }
 
 }

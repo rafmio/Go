2d10c740f4cdd9a5782c40b9ea9cafb811da1048
Rafael Idrisov <rafael.idrisov@gmail.com>
2022-12-24 19:14:48 +0300

    Home PC 24.12.2022 19:14

---
+++ src/pro-go/htmltext/htmltext18/main.go
@@ -0,0 +1,44 @@
+// Using template variables in ranve actions
+package main
+
+import (
+	"html/template"
+	"os"
+	"strings"
+)
+
+func GetCategories(products []Product) (categories []string) {
+	catMap := map[string]string{}
+	for _, p := range products {
+		if (catMap[p.Category] == "") {
+			catMap[p.Category] = p.Category
+			categories = append(categories, p.Category)
+		}
+	}
+	return
+}
+
+func Exec(t *template.Template) error {
+	productMap := map[string]Product {}
+	for _, p := range Products {
+		productMap[p.Name] = p
+	}
+	return t.Execute(os.Stdout, &productMap)
+}
+
+
+func main() {
+	allTemplates := template.New("allTempates")
+	allTemplates.Funcs(map[string]interface{} {
+		"getCats": GetCategories,
+		"lower": strings.ToLower,
+	})
+
+	allTemplates, err := allTemplates.ParseGlob("templates/*.html")
+	if (err == nil) {
+		selecetedTemplates := allTemplates.Lookup("mainTemplate")
+		err = Exec(selecetedTemplates)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+}

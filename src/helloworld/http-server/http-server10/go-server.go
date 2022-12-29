ab5defb494e845d1e1b2abb052c4c2185dff5667
Rafael Idrisov <rafael.idrisov@gmail.com>
2022-12-29 07:24:10 +0300

    Home PC 29.12.2022 07:23

--- src/helloworld/http-server/http-server10/go-server.go
+++ src/helloworld/http-server/http-server10/go-server.go
@@ -1,18 +1,50 @@
 // https://medium.com/rungo/secure-https-servers-in-go-a783008b36da
+// package main
+//
+// import (
+// 	"net/http"
+// 	"fmt"
+// 	"log"
+// )
+//
+// func main() {
+// 	// handle `/` route
+// 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+// 		fmt.Fprint(res, "Hello World")
+// 	})
+//
+// 	// run server on port 9000
+// 	log.Fatal(http.ListenAndServeTLS(":9000", "localhost.crt", "localhost.key", nil))
+// }
+
 package main
 
 import (
 	"net/http"
 	"fmt"
 	"log"
+	"crypto/tls"
 )
 
 func main() {
+	// generate a `Certificate` struct
+	cert, err := tls.LoadX509KeyPair("localhost-2022-12-28-210019.cer", "localhost-2022-12-28-210019.pkey")
+	if (err != nil) {
+		fmt.Println(err.Error())
+	}
+	s := &http.Server{
+		Addr: ":9000",
+		Handler: nil,			// user `http.DefaultServeMux`
+		TLSConfig: &tls.Config {
+			Certificates: []tls.Certificate{cert},
+		},
+	}
+
 	// handle `/` route
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello World")
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request){
+		fmt.Fprint(res, "Hello, Custom World!")
 	})
 
 	// run server on port 9000
-	log.Fatal(http.ListenAndServeTLS(":9000", "localhost.crt", "localhost.key", nil))
+	log.Fatal(s.ListenAndServeTLS("", ""))
 }

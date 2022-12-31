785e2b0793fa7c0b6cd05755557271ad4c61a9a6
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-01-01 02:20:54 +0300

    Home PC 01.01.2023 02:20

---
+++ src/golangify/snippetbox/snippetbox10/cmd/web/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "path/filepath"
+)
+
+func main() {
+  mux := http.NewServeMux()
+
+  fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+  mux.Handle("/static", http.NotFoundHandler())
+  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+  err := http.ListenAndServe(":4000", mux)
+  log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+  fs http.FileSystem
+}
+
+
+// Open() вызывается каждый раз, когда http.FileServer получает запрос
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+  f, err := nfs.fs.Open(path)
+  if err != nil {
+    return nil, err
+  }
+
+  s, err := f.Stat()
+  if s.IsDir() {          // Если это папка, то с пом. Stat() проверим наличие index.html внутри
+    index := filepath.Join(path, "index.html")
+    if _, err := nfs.fs.Open(index); err != nil {
+      closeErr := f.Close() // Закрываем чтобы избежать утечки файлового дескриптора
+      if closeErr != nil {
+        return nil, closeErr
+      }
+      return nil, err
+    }
+  }
+  return f, nil
+}

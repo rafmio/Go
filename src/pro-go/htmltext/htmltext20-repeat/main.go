042645760b0d6f8f20ed7668d764b686fa779d2b
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-01-09 16:16:18 +0300

    Home PC 09.01.2023 16:16

--- src/pro-go/htmltext/htmltext20-repeat/main.go
+++ src/pro-go/htmltext/htmltext20-repeat/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
   "html/template"
-  // "os"
+  "os"
 )
+
+func Exec(t *template.Template) error {
+  return t.Execute(os.Stdout, &Kayak)
+}
 
 func main() {
   allTemplates, err := template.ParseGlob("templates/*.html")
   if (err == nil) {
-    for _, t := range allTemplates.Templates() {
-      Printfln("Template name: %v", t.Name())
-    }
-  } else {
+    selectedTemplated := allTemplates.Lookup("template.html")
+    err = Exec(selectedTemplated)
+  }
+  if (err != nil) {
     Printfln("Error: %v", err.Error())
   }
 }

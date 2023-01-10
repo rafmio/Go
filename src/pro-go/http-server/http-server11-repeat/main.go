4392efe421d25f49f25d1c5e486dbeba80e9f902
rafmio <rafmio@yandex.ru>
2023-01-10 10:37:10 +0300

    Work 10.01.2023 10:37

--- src/pro-go/http-server/http-server11-repeat/main.go
+++ src/pro-go/http-server/http-server11-repeat/main.go
@@ -1,8 +1,29 @@
 package main
 
+import (
+  "net/http"
+  "io"
+)
+
+type StringHandler struct {
+  message string
+}
+
+func (sh StringHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  Printfln("Request for %v", r.URL.Path)
+  switch r.URL.Path {
+  case "/favico.ico":
+    http.NotFound(w, r)
+  case "/message":
+    io.WriteString(w, sh.message)
+  default:
+    http.Redirect(w, r, "/message", http.StatusTemporaryRedirect)
+  }
+}
+
 func main() {
-  for _, p := range Products {
-    Printfln("Product: %v, Category: %v, Price: %v",
-    p.Name, p.Category, p.Price)
+  err := http.ListenAndServe(":5000", StringHandler {message: "<p>Susi-Musi, Tosi-Bosi</p>"})
+  if (err != nil) {
+    Printfln("Error: %v", err.Error())
   }
 }

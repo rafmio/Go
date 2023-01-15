130f5911365678caa6b537897bdbf28703f31ccf
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-01-15 19:32:17 +0300

    Home PC 15.01.2023 19:32

--- src/helloworld/http-server/http-server15/main.go
+++ src/helloworld/http-server/http-server15/main.go
@@ -1,18 +1,30 @@
-// https://www.alexedwards.net/blog/serving-static-sites-with-go
+// CODE DOESN'T WORK!!!
 package main
 
 import (
-  "log"
-  "net/http"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("./static"))
-  http.Handle("/", fs)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-  log.Print("Listening on :5000...")
-  err := http.ListenAndServe(":5000", nil)
-  if (err != nil) {
-    log.Fatal(err)
-  }
+	http.HandleFunc("/", serveTemplate)
+
+	log.Print("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }

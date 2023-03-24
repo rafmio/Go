3ff287e83e2be65ef50f100007122ecaabf07915
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-03-24 08:44:28 +0300

    Work 24.03.2023 08:44

---
+++ src/go-for-profi1/works06/pointers.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func getPointer(n *int) {
+	*n = *n * *n
+}
+
+func returnPointer(n int) *int {
+	v := n * n
+	return &v
+}
+
+func main() {
+	i := -10
+	j := 25
+	pI := &i
+	pJ := &j
+
+	fmt.Println("pI memory: ", pI)
+	fmt.Println("pI memory: ", pJ)
+	fmt.Println("pI value: ", *pI)
+	fmt.Println("pI value: ", *pJ)
+
+	v := returnPointer(10)
+	fmt.Println("v: ", v, "*v: ", *v)
+	*v += 200
+	fmt.Println("*v: ", *v)
+
+	*pI = 123456
+	*pI--
+	fmt.Println("i: ", i)
+
+	getPointer(pJ)
+	fmt.Println("j: ", j)
+	k := returnPointer(12)
+	fmt.Println(*k)
+	fmt.Println(k)
+}

f6a6c5527279a26adb3b15797ea2971870ac6acc
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-04-04 07:32:44 +0300

    Home PC 04.04.2023 07:32

---
+++ src/kontur-qualifying/famous-artist-ex.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	var num int
+	daysStr := make([]string, 0)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Fscan(reader, &num)
+	for i := 0; i < num; i++ {
+		var day string
+		fmt.Fscan(reader, &day)
+		daysStr = append(daysStr, day)
+	}
+
+	days := make([]int, 0)
+	for _, value := range daysStr {
+		day, _ := strconv.Atoi(value)
+		days = append(days, day)
+	}
+
+	sortedDays := make([]int, len(days))
+	copy(sortedDays, days)
+	sort.Ints(sortedDays)
+
+	var max, min int
+	min = sortedDays[0]
+	max = sortedDays[len(sortedDays)-1]
+
+	contrastLtR := make(map[int]int)
+	for idx, value := range days {
+		if value == min {
+			contrastLtR[min] = idx
+			break
+		}
+	}
+
+	for idx, value := range days {
+		if value == max {
+			contrastLtR[max] = idx
+		}
+	}
+
+	contrastRtL := make(map[int]int)
+	for idx, value := range days {
+		if value == max {
+			contrastRtL[max] = idx
+			break
+		}
+	}
+
+	for idx, value := range days {
+		if value == min {
+			contrastRtL[min] = idx
+		}
+	}
+
+	slsOutput := make([]int, 0)
+
+	difLtR := contrastLtR[max] - contrastLtR[min]
+	difRtL := contrastRtL[max] - contrastRtL[min]
+
+	if difLtR > difRtL {
+		slsOutput = append(slsOutput, contrastLtR[max]+1)
+		slsOutput = append(slsOutput, contrastLtR[min]+1)
+	} else {
+		slsOutput = append(slsOutput, contrastRtL[max]+1)
+		slsOutput = append(slsOutput, contrastRtL[min]+1)
+	}
+	fmt.Println(slsOutput)
+}

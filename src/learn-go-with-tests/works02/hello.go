c5544b197212621a04f9d30af5a7891ae9fab1ef
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-04-08 22:56:28 +0300

    Work 08.04.2023 22:56

---
+++ src/learn-go-with-tests/works02/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}

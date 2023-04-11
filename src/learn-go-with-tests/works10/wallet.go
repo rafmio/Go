f6ac2f3b4735197b7ac7aadf22bcb206370e9dde
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-04-12 00:03:40 +0300

    Home PC 12.04.2023 00:03

---
+++ src/learn-go-with-tests/works10/wallet.go
@@ -0,0 +1,36 @@
+package works10
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	// return (*w).balance
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}

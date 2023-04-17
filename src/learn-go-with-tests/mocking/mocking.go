a2f6a427821c354327a9e9ecb39f35f0f822f7cd
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-04-17 21:00:40 +0300

    Home PC 17.04.2023 21:00

--- src/learn-go-with-tests/mocking/mocking.go
+++ src/learn-go-with-tests/mocking/mocking.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
+	"time"
 )
 
-func Countdown(out *bytes.Buffer) {
-	fmt.Fprint(out, "3")
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprintln(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
 }

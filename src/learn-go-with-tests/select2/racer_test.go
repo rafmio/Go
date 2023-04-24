64e2d4afb96ac9a7ab8c492dd575fa385a5c21ea
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-04-24 23:49:31 +0300

    Home PC 24.04.2023 23:49

---
+++ src/learn-go-with-tests/select2/racer_test.go
@@ -0,0 +1,33 @@
+package racer
+
+import (
+	// "fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRaser(t *testing.T) {
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an arror but didn't get one")
+		}
+	})
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}

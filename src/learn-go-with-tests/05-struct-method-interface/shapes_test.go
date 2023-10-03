c1b6fe7d10505a281ed289d2288d1a8c8b285cf2
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-10-03 21:59:41 +0300

    Home PC 03.10.2023 21:59

--- src/learn-go-with-tests/05-struct-method-interface/shapes_test.go
+++ src/learn-go-with-tests/05-struct-method-interface/shapes_test.go
@@ -2,16 +2,6 @@ package shapes
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
-}
-
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}

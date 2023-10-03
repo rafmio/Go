d3a72f26110400633ccef977f5a95d872710cc91
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-10-03 21:41:25 +0300

    Home PC 03.10.2023 21:41

--- src/learn-go-with-tests/05-struct-method-interface/shapes_test.go
+++ src/learn-go-with-tests/05-struct-method-interface/shapes_test.go
@@ -1,8 +1,6 @@
-package works07
+package shapes
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -15,21 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := Area(rectangle)
+		want := 72.0
+
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
-	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	})
 }

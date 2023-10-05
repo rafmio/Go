ef7b1c031d8b0befce5571704d25353977432650
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-10-05 15:46:39 +0300

    Home PC 05.10.2023 15:46

--- src/learn-go-with-tests/05-struct-method-interface/shapes_test.go
+++ src/learn-go-with-tests/05-struct-method-interface/shapes_test.go
@@ -3,23 +3,38 @@ package shapes
 import "testing"
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		got := Area(rectangle)
-		want := 72.0
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := Area(circle)
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("got %g want %g", got, tt.hasArea)
+		}
+	}
 }

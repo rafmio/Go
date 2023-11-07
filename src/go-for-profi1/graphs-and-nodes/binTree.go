9a353ebe76263791f1dc3e55d0c60c41b3a3535e
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-11-07 15:02:44 +0300

    Home PC 07.11.2023 15:02

---
+++ src/go-for-profi1/graphs-and-nodes/binTree.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Tree struct {
+	Left  *Tree
+	Value int
+	Right *Tree
+}
+
+func traverse(t *Tree) {
+	if t == nil {
+		return
+	}
+
+	traverse(t.Left)
+	fmt.Println(t.Value, " ")
+	traverse(t.Right)
+}
+
+func create(n int) *Tree {
+	var t *Tree
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < 2*n; i++ {
+		temp := rand.Intn(n * 2)
+		t = insert(t, temp)
+	}
+
+	return t
+}

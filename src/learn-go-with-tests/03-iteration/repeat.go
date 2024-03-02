236e978c378e4fff0b9d3dfca84b629781586992
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-03-02 16:02:22 +0300

    Home PC 02.03.2024 16:02

--- src/learn-go-with-tests/03-iteration/repeat.go
+++ src/learn-go-with-tests/03-iteration/repeat.go
@@ -5,7 +5,7 @@ const repeatCount = 5
 func Repeat(character string) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
-		repeated = repeated + character
+		repeated += character
 	}
 
 	return repeated
--- src/learn-go-with-tests/03-iteration/repeat_test.go
+++ src/learn-go-with-tests/03-iteration/repeat_test.go
@@ -1,33 +1,23 @@
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/iteration
 package iteration
 
 import (
-	"fmt"
-	"os"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
-	expecetd := "aaaaa"
+	expected := "aaaaa"
 
-	if repeated != expecetd {
-		t.Errorf("expected %q but got %q", expecetd, repeated)
-	} else {
-		fmt.Fprintf(os.Stdout, "repeated: %s\n", repeated)
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
-func BenchmarkRepeat(b *testing.B) {
+func BechmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("Y")
+		Repeat("a")
 	}
 }
 
 // go test -bench=.
-
-func ExampleRepeat() {
-	res := Repeat("Hello-mello")
-	fmt.Println(res)
-}
-
-// go test -v

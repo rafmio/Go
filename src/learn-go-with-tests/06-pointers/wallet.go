eb183d17a70bfdf46f46ad0ee27b08082cb0b561
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-03-03 20:12:50 +0300

    Home PC 03.03.2024 20:12

---
+++ src/learn-go-with-tests/06-pointers/wallet.go
@@ -0,0 +1,17 @@
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors
+package wallet
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %p\n", &w.balance)
+	w.balance += amount
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}

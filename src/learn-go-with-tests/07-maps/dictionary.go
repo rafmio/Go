51981350b1193e3c904fa0e9f73c6ba7079d082b
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-03-13 23:03:50 +0300

    Home PC 13.03.2024 23:03

---
+++ src/learn-go-with-tests/07-maps/dictionary.go
@@ -0,0 +1,22 @@
+package mapsdict
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
+
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#pointers-copies-et-al

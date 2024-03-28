a877da093b32ce3eb81de92b87640e69ee0a03b7
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-03-28 20:11:49 +0300

    Home PC 28.03.2024 20:11

--- src/learn-go-with-tests/14-context/context.go
+++ src/learn-go-with-tests/14-context/context.go
@@ -5,28 +5,14 @@ import (
 	"net/http"
 )
 
-// Store fetches data.
+// Store fetches data
 type Store interface {
 	Fetch() string
-	Cancel()
 }
 
-// Server returns a handler for calling Store.
+// Server returns a handler for calling Store
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
-		}
+		fmt.Fprint(w, store.Fetch())
 	}
 }
--- src/learn-go-with-tests/14-context/context_test.go
+++ src/learn-go-with-tests/14-context/context_test.go
@@ -1,46 +1,29 @@
 package ctxt
 
 import (
-	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 )
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
 
 func TestServer(t *testing.T) {
 	data := "hello, world"
+	svr := Server(&StubStore{data})
 
-	t.Run("returns data from store", func(t *testing.T) {
-		store := &SpyStore{response: data, t: t}
-		svr := Server(store)
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
 
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
+	svr.ServeHTTP(response, request)
 
-		svr.ServeHTTP(response, request)
-
-		if response.Body.String() != data {
-			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-		}
-
-		store.assertWasNotCancelled()
-	})
-
-	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		store := &SpyStore{response: data, t: t}
-		svr := Server(store)
-
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-
-		cancellingCtx, cancel := context.WithCancel(request.Context())
-		time.AfterFunc(5*time.Millisecond, cancel)
-		request = request.WithContext(cancellingCtx)
-
-		response := httptest.NewRecorder()
-
-		svr.ServeHTTP(response, request)
-
-		store.assertWasCancelled()
-	})
+	if response.Body.String() != data {
+		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+	}
 }

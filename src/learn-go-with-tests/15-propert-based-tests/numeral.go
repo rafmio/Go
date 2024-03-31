bd2420cd3164497f4092680a65e4b36e76d42965
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-03-31 15:51:50 +0300

    Home PC 31.03.2024 15:52

--- src/learn-go-with-tests/15-propert-based-tests/numeral.go
+++ src/learn-go-with-tests/15-propert-based-tests/numeral.go
@@ -1,13 +1,44 @@
 package roman
 
-import "strings"
+import (
+	"strings"
+)
+
+type RomanNumerals struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{8, "VIII"},
+	{7, "VII"},
+	{6, "VI"},
+	{5, "V"},
+	{4, "IV"},
+	{3, "III"},
+	{2, "II"},
+	{1, "I"},
+}
 
 func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
-		result.WriteString("I")
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
 	}
 
 	return result.String()

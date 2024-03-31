f9ab6136bad581b4bfbdf8aeb27be9778288dd0c
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-03-31 14:46:41 +0300

    Home PC 31.03.2024 14:46

---
+++ src/learn-go-with-tests/15-propert-based-tests/numeral_test.go
@@ -0,0 +1,28 @@
+package roman
+
+import "testing"
+
+func TestRomanNuberals(t *testing.T) {
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		// {"4 gets converted to IV", 4, "IV"},
+		// {"5 gets converted to V", 5, "V"},
+		// {"6 gets converted to VI", 6, "VI"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
+
+}

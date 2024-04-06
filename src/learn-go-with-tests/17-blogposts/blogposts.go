df530931ed88134688f30be800592232e7459975
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-06 13:24:01 +0300

    Home PC 06.04.2024 13:23

--- src/learn-go-with-tests/17-blogposts/blogposts.go
+++ src/learn-go-with-tests/17-blogposts/blogposts.go
@@ -7,5 +7,36 @@ import (
 // NewPostsFromFS return a collection of blog posts from a file system.
 // If it does not conform to the format then it'll return an error
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+
+	// ReadDir reads the named directory and returns a list of
+	// directory entries sorted by filename.
 	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err // TODO: needs clarification, should we totally fail if one file fails? or just ignore?
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
 }
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)
+}
+
+// Where can fs.FS can come from?
+// For e.g.:
+// dir := os.DirFS("/path/to/directory")
--- src/learn-go-with-tests/17-blogposts/blogposts_test.go
+++ src/learn-go-with-tests/17-blogposts/blogposts_test.go
@@ -1,21 +1,66 @@
-package blogposts_test
+package main
 
 import (
+	"blogposts"
+	"reflect"
 	"testing"
 	"testing/fstest"
-
-	blogposts "github.com/quii/learn-go-with-tests/reading-files"
 )
 
-func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello_world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+func TestNewBlogPost(t *testing.T) {
+	const (
+		firstBody = `Title: Post1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
+	fs := fsTestMapFS{
+		"hello_world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
-	posts, _ := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromFS(fs)
 
+	assertNoError(t, err)
+
+	assertPostsLength(t, posts, fs)
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+	World`,
+	})
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func assertPostsLength(t *testing.T, posts []blogposts.Post, fs fstest.MapFS) {
+	t.Helper()
 	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, wanted %d ponsts", len(posts), len(fs))
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }

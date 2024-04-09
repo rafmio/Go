fe104e9845cb5540b86095da7165c480cf7a7b4f
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-10 00:37:45 +0300

    Home PC 10.04.2024 00:37

--- src/99-problems/p09-pack-duplicates/p09-pack-duplicate.go
+++ src/99-problems/p09-pack-duplicates/p09-pack-duplicate.go
@@ -5,6 +5,18 @@ package packdup
 // single one element
 func PackDupStr(list []string) [][]string {
 	packedDupListStr := make([][]string, 0)
+	uniqueList := make([]string, 0)
+
+	for i := 0; i < len(list)-1; i++ {
+		if len(uniqueList) == 0 {
+			uniqueList = append(uniqueList, list[i])
+		} else if list[i] == uniqueList[0] {
+			uniqueList = append(uniqueList, list[i])
+		} else if list[i] != uniqueList[0] {
+			packedDupListStr = append(packedDupListStr, uniqueList)
+			uniqueList = uniqueList[:0]
+		}
+	}
 
 	return packedDupListStr
 }

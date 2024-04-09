d33b4f128ba8f0e49717230bbfd601b656f93f64
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-09 22:29:04 +0300

    Home PC 09.04.2024 22:29

---
+++ src/learn-go-with-tests/20-testing-fundamentals/main.go
@@ -0,0 +1,24 @@
+// https://quii.gitbook.io/learn-go-with-tests/testing-fundamentals/intro-to-acceptance-tests#how-to-write-basic-acceptance-tests
+package acceptancetests
+
+import (
+	"context"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var (
+		ctx        = context.Background()
+		httpServer = &http.Server{Addr: ":8080", Handler: http.HandlerFunc(acceptancetests.SlowHandler)}
+		server     = gracefulshutdown.NewServer(httpServer)
+	)
+
+	if err := server.ListenAndServe(ctx); err != nil {
+		// this will typically happen if our responses aren't written before the ctx deadline, not much can be done
+		log.Fatalf("uh oh, didnt shutdown gracefully, some responses may have been lost %v", err)
+	}
+
+	// hopefully, you'll always see this instead
+	log.Println("shutdown gracefully! all responses were sent")
+}
---
+++ src/learn-go-with-tests/20-testing-fundamentals/running.go
@@ -0,0 +1,6 @@
+// https://quii.gitbook.io/learn-go-with-tests/testing-fundamentals/intro-to-acceptance-tests#building-and-running-the-program
+package acceptancetests
+
+const (
+	baseBinName = "temp-testbinary"
+)

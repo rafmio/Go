ca04d1be120af149a0c7dc437ba8853f28de1345
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-10 23:56:55 +0300

    Home PC 10.04.2024 23:56

--- src/99-problems/p09-pack-duplicates/p09-pack-duplicate.go
+++ src/99-problems/p09-pack-duplicates/p09-pack-duplicate.go
@@ -3,20 +3,26 @@ package packdup
 // PackDupStr accepts a string slice ([]string) pack duplicates into it's own
 // slice and do similar with unique elements - pack them into own slice with
 // single one element
-func PackDupStr(list []string) [][]string {
-	packedDupListStr := make([][]string, 0)
-	uniqueList := make([]string, 0)
 
-	for i := 0; i < len(list)-1; i++ {
-		if len(uniqueList) == 0 {
-			uniqueList = append(uniqueList, list[i])
-		} else if list[i] == uniqueList[0] {
-			uniqueList = append(uniqueList, list[i])
-		} else if list[i] != uniqueList[0] {
-			packedDupListStr = append(packedDupListStr, uniqueList)
-			uniqueList = uniqueList[:0]
+func PackDupStr(list []string) [][]string {
+	var result [][]string
+	// currentGroup := []string{}
+	currentGroup := make([]string, 0)
+
+	for i, letter := range list {
+		if i == 0 || list[i-1] != letter { // TODO: проверить не будет ли out of range при list[i-1]
+			// Если текущая буква отличается от предыдущей или это первый элемент, начинаем новый слайс
+			currentGroup = []string{letter}
+		} else {
+			// Добавляем букву к текущему слайсу
+			currentGroup = append(currentGroup, letter)
+		}
+
+		// Добавляем текущий слайс в результат, если мы достигли конца списка или следующая буква другая
+		if i+1 >= len(list) || list[i+1] != letter {
+			result = append(result, currentGroup)
 		}
 	}
 
-	return packedDupListStr
+	return result
 }

a49452d9e9005ffbb46e303a94f0ef1e6a641c17
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-10 22:14:57 +0300

    Home PC 10.04.2024 22:14

--- src/learn-go-with-tests/21-build-an-application/server_test.go
+++ src/learn-go-with-tests/21-build-an-application/server_test.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,33 +10,33 @@ import (
 
 func TestGETPlayers(t *testing.T) {
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
-		response := httptest.NewRecorder() // returns *ResponseRecorder
-
-		PlayerServer(response, request)
-
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
-	t.Run("return Floyd's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "10"
+		assertResponseBody(t, response.Body.String(), "20")
+	})
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
 	})
 }
 
-// last step:
-// https://quii.gitbook.io/learn-go-with-tests/build-an-application/http-server#write-enough-code-to-make-it-pass-1
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("response body is wrong, got %q, want %q", got, want)
+	}
+}

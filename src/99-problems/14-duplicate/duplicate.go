1d7d7cfc7524962ea462cff12d8369334e96cd8b
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-15 18:44:08 +0300

    Home PC 15.04.2024 18:44

--- src/99-problems/14-duplicate/duplicate.go
+++ src/99-problems/14-duplicate/duplicate.go
@@ -2,14 +2,19 @@ package duplicate
 
 func Duplicate(list []string) []string {
 
-	result := make([]string, 0)
+	// result := make([]string, 0)
+	result := make([]string, 0, len(list)*2)
 	if len(list) == 0 {
 		return result
 	}
 
-	for i := 0; i < len(list); i++ {
-		result = append(result, list[i])
-		result = append(result, list[i])
+	// for i := 0; i < len(list); i++ {
+	// 	result = append(result, list[i])
+	// 	result = append(result, list[i])
+	// }
+
+	for _, value := range list {
+		result = append(result, value, value)
 	}
 
 	return result

9d66179ea91ee7b110996acf2c90784d2141a72f
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-04-18 18:50:19 +0300

    Home PC 18.04.2024 18:50

---
+++ src/99-problems/p16-drop-nth-elem/p16-drop-element_test.go
@@ -0,0 +1,42 @@
+package dropelem
+
+import (
+	"testing"
+)
+
+type TstCase struct {
+	input    []string
+	nThElem  int
+	expected []string
+	err      error
+}
+
+// test function for DropElem() than drops every N'th element from a list
+func TestDropElem(t *testing.T) {
+	tstCases := []TstCase{
+		{
+			input:    []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"},
+			nThElem:  2,
+			expected: []string{"a", "c", "e", "g", "i", "k", "m", "o", "q", "s", "u", "w", "y"},
+			err:      nil,
+		},
+		{
+			input:    []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},
+			nThElem:  3,
+			expected: []string{"1", "2", "4", "5", "7", "8", "10"},
+			err:      nil,
+		},
+		{
+			input:    []string{"a", "b"},
+			nThElem:  0,
+			expected: []string{"a", "b"},
+			err:      nil,
+		},
+		{
+			input:    []string{"a", "b", "c"},
+			nThElem:  -1,
+			expected: []string{"a", "b", "c"},
+			err:      err,
+		},
+	}
+}

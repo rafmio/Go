51e77c246013e57a2253f6fa6b66212139a14f64
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-05-07 19:44:08 +0300

    Home PC 07.05.2024 19:44

---
+++ src/package-doc/strings/01-type-Builder.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var b strings.Builder
+	for i := 3; i >= 1; i-- {
+		fmt.Fprintf(&b, "%d...", i)
+	}
+
+	b.WriteString("ignition")
+	fmt.Println(b.String())
+
+	fmt.Println()
+	var aA strings.Builder
+	fmt.Fprintf(&aA, "%s", "Hehehe, it's cool")
+	fmt.Println(aA)
+}

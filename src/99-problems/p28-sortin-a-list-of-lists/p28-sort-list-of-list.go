bb0ed4ab3968fdaebef1c7976a0432c9c8c49f4f
rafmio <rafmio@yandex.ru>
2024-05-10 12:25:16 +0300

    99 pp p28 done

--- src/99-problems/p28-sortin-a-list-of-lists/p28-sort-list-of-list.go
+++ src/99-problems/p28-sortin-a-list-of-lists/p28-sort-list-of-list.go
@@ -14,18 +14,13 @@ func SortListOfList(list [][]string) ([][]string, error) {
 	var isOrdered bool
 
 	for !isOrdered {
-		for i := 0; i < len(list); i++ {
-			if i < len(list)-1 {
-				if len(list[i]) > len(list[i+1]) {
-					list[i], list[i+1] = list[i+1], list[i]
-					i = 0
-					isOrdered = false
-				} else {
-					isOrdered = true
-				}
+		isOrdered = true
+		for i := 0; i < len(list)-1; i++ { // Уменьшили верхнюю границу до len(list)-1
+			if len(list[i]) > len(list[i+1]) {
+				list[i], list[i+1] = list[i+1], list[i]
+				isOrdered = false
 			}
 		}
-
 	}
 
 	return list, nil

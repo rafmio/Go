e7af49500bf7c3822e7023a938cbad0f9a2fa95a
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-08-15 23:52:11 +0300

    Home PC 15.08.2024 23:51 connection to DB for logtracker's API

--- src/helloworld/dataBases/01-db-connection.go
+++ src/helloworld/dataBases/01-db-connection.go
@@ -2,11 +2,64 @@ package main
 
 import (
 	"database/sql"
-	"net/http"
+	"fmt"
+	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
-func fetchHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("postgres", "host")
+type LogEntry struct {
+	TmStmp time.Time
+	SrcIP  string
+	Len    string
+	Ttl    string
+	Id     string // will named 'inner id' in database
+	Spt    string
+	Dpt    string
+	Window string // will named 'wndw' in database
+}
+
+func main() {
+	db, err := sql.Open("postgres", "host=194.58.102.129 port=5432 dbname=logtracker user=raf password=qwq121 sslmode=disable")
+	if err != nil {
+		log.Println("Open DB:", err)
+		return
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("Ping DB:", err)
+		return
+	}
+
+	fmt.Println("Successfully connected!")
+
+	rows, err := db.Query("SELECT * FROM logs WHERE tmstmp BETWEEN '2022-06-01' AND '2022-06-30'")
+	if err != nil {
+		log.Println("Query DB:", err)
+		return
+	}
+
+	defer rows.Close()
+
+	logEnties := make([]LogEntry, 0)
+
+	for rows.Next() {
+		// var logID, level, message string
+		// err = rows.Scan(&logID, &level, &message)
+		// if err != nil {
+		// 	log.Println("Scan row:", err)
+		// 	return
+		// }
+		// fmt.Printf("ID: %s, Level: %s, Message: %s\n", logID, level, message)
+		var entry LogEntry
+		err = rows.Scan(&entry.TmStmp, &entry.SrcIP, &entry.Len, &entry.Ttl, &entry.Id, &entry.Spt, &entry.Dpt, &entry.Window)
+		if err != nil {
+			log.Println("Scan row:", err)
+			return
+		}
+		logEnties = append(logEnties, entry)
+	}
 }

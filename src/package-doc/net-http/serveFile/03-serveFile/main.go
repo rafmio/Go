54617a028279519711139753c6ae082918e4fad7
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-09-18 22:27:36 +0300

    Home PC 18.09.2024 22:27

---
+++ src/package-doc/net-http/serveFile/03-serveFile/main.go
@@ -0,0 +1,14 @@
+// https://golang.howtos.io/serving-html-files-with-go-s-net-http-package/
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}

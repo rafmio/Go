a7cf5a68c54191cffc067f5562192ae31c2b1901
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-10-15 23:08:37 +0300

    Home PC 15.10.2024 23:08 channels

--- src/helloworld/goroutines/15-goroutines.go
+++ src/helloworld/goroutines/15-goroutines.go
@@ -21,7 +21,9 @@ func main() {
 
 	filesToRead := make([]string, 0)
 	var fileToWrite string
+
 	fileNamesChan := make(chan string)
+	defer close(fileNamesChan)
 
 	var wg sync.WaitGroup
 
@@ -49,11 +51,44 @@ func main() {
 		}
 	}()
 
-	go func() {
-		wg.Wait()
-		close(fileNamesChan)
-	}()
+	wg.Wait()
 
 	fmt.Println(filesToRead, fileToWrite)
 
+	bytesForWrite := make(chan []byte)
+	defer close(bytesForWrite)
+
+	for _, fileToRead := range filesToRead {
+		wg.Add(1)
+		go func(fileToRead string) {
+			defer wg.Done()
+
+			bytes, err := os.ReadFile(fileToRead)
+			if err != nil {
+				log.Println(err)
+			}
+
+			bytesForWrite <- bytes
+
+			log.Println("bytes has been sent to channel")
+		}(fileToRead)
+	}
+
+	var wwg sync.WaitGroup
+
+	go func() {
+		wwg.Add(1)
+		for bytes := range bytesForWrite {
+			err := os.WriteFile(fileToWrite, bytes, 0644)
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Println("file has been written")
+			}
+		}
+		wwg.Done()
+	}()
+
+	wwg.Wait()
+	wg.Wait()
 }

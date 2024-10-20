e7cb8a98d595f36f0ca2989e8e443b6752f68f60
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-10-20 15:28:12 +0300

    Home PC 20.10.2024 15:27 channels, concurrency exersices

--- src/go-for-profi/concurrency/09-exercises/01-exercise.go
+++ src/go-for-profi/concurrency/09-exercises/01-exercise.go
@@ -73,6 +73,81 @@ func makeRawDataChan(fileNamesSls *[]string) <-chan []byte {
 	return rawDataChan
 }
 
+func makeEntriesChan(rawDataSls *[][]byte) <-chan string {
+	entriesChan := make(chan string)
+	var wg sync.WaitGroup
+
+	for _, byteSls := range *rawDataSls {
+		wg.Add(1)
+		go func(byteSls []byte) {
+			defer wg.Done()
+			entries := strings.Split(string(byteSls), "\n")
+			for _, entry := range entries {
+				entriesChan <- entry
+			}
+		}(byteSls)
+	}
+
+	go func() {
+		wg.Wait()
+		close(entriesChan)
+	}()
+
+	return entriesChan
+}
+
+func makeSplittedStrChan(entriesSls *[]string) <-chan []string {
+	splittedStrChan := make(chan []string, len(*entriesSls)/2)
+	var wg sync.WaitGroup
+
+	for _, entry := range *entriesSls {
+		wg.Add(1)
+		go func(entry string) {
+			defer wg.Done()
+			splittedStr := strings.Fields(entry)
+			splittedStrChan <- splittedStr
+		}(entry)
+	}
+
+	go func() {
+		wg.Wait()
+		close(splittedStrChan)
+	}()
+
+	return splittedStrChan
+}
+
+func makeIpPortTupleChan(splittedStrChan <-chan []string) <-chan []string {
+	fmt.Println("Enter the makeIpProtTupleChan()")
+	ipPortTupleChan := make(chan []string, len(splittedStrChan))
+
+	var wg sync.WaitGroup
+
+	for splittedStr := range splittedStrChan {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ipPortTuple := make([]string, 2)
+			for _, filed := range splittedStr {
+				if strings.HasPrefix(filed, "SRC=") {
+					ipPortTuple[0] = strings.TrimPrefix(filed, "SRC=")
+				}
+				if strings.HasPrefix(filed, "DPT=") {
+					ipPortTuple[1] = strings.TrimPrefix(filed, "DPT=")
+				}
+			}
+			ipPortTupleChan <- ipPortTuple
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(ipPortTupleChan)
+	}()
+
+	return ipPortTupleChan
+}
+
 func main() {
 	fileNamesChan := makeFileNamesChan()
 	fileNamesSls := make([]string, 0)
@@ -101,4 +176,31 @@ func main() {
 	} else {
 		fmt.Println("rawDataSls is incomplete, len(rawDataSls):", len(rawDataSls))
 	}
+
+	entriesSls := make([]string, 0)
+	entriesChan := makeEntriesChan(&rawDataSls)
+
+	for entry := range entriesChan {
+		entriesSls = append(entriesSls, entry)
+		// fmt.Println(entry)
+	}
+
+	fmt.Println("len(entriesSls):", len(entriesSls))
+
+	uniqueIp := make(map[string]int)   // map[string(IP)]count
+	uniquePort := make(map[string]int) // map[string(Port)]count
+
+	splittedStrChan := makeSplittedStrChan(&entriesSls)
+
+	ipPortTupleChan := makeIpPortTupleChan(splittedStrChan)
+
+	counter = 0
+
+	for ipPortTuple := range ipPortTupleChan {
+		uniqueIp[ipPortTuple[0]]++
+		uniquePort[ipPortTuple[1]]++
+	}
+
+	fmt.Println("len(uniqueIp):", len(uniqueIp))
+	fmt.Println("len(uniquePort):", len(uniquePort))
 }

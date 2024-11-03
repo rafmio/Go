5830cf33090e2a38728ac2bddb577df6adeb9a47
Rafael Idrisov <rafael.idrisov@gmail.com>
2024-11-03 11:23:23 +0300

    Home PC 03.11.2024 11:23 import

---
+++ src/helloworld/import/01-works/cmd/callOne.go
@@ -0,0 +1,9 @@
+package cmd
+
+import (
+	"fmt"
+)
+
+func callOne() {
+	fmt.Println("Function callOne() executed")
+}
---
+++ src/helloworld/import/01-works/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"api/cmd"
+)
+
+func main() {
+	fmt.Println("main() launched")
+
+	cmd.callOne()
+}

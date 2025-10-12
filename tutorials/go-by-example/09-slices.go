a8bbfcd9b92c86d54e14669b777705c92f86950c
Rafael Idrisov <rafael.idrisov@gmail.com>
2025-10-12 17:21:46 +0300

    Home PC 12.10.2025 17:21 hw

---
+++ tutorials/go-by-example/09-slices.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+}

6b3b74fdf2289db9c06e54a32e820ce354230d7f
Rafael Idrisov <rafael.idrisov@gmail.com>
2025-11-09 12:44:17 +0300

    Home PC 09.11.2025 12:44

--- tutorials/go-by-example/25-iterators.go
+++ tutorials/go-by-example/25-iterators.go
@@ -1,6 +1,12 @@
 // https://gobyexample.com/range-over-iterators
 package main
 
+import (
+	"fmt"
+	"iter"
+	"slices"
+)
+
 type List[T any] struct {
 	head, tail *element[T]
 }
@@ -8,4 +14,58 @@ type List[T any] struct {
 type element[T any] struct {
 	next *element[T]
 	val  T
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) All() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for e := lst.head; e != nil; e = e.next {
+			if !yield(e.val) {
+				return
+			}
+		}
+	}
+}
+
+func genFib() iter.Seq[int] {
+	return func(yield func(int) bool) {
+		a, b := 1, 1
+
+		for {
+			if !yield(a) {
+				return
+			}
+			a, b = b, a+b
+		}
+	}
+}
+
+func main() {
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+
+	for e := range lst.All() {
+		fmt.Println(e)
+	}
+
+	all := slices.Collect(lst.All())
+	fmt.Println("all:", all)
+
+	for n := range genFib() {
+		if n >= 10 {
+			break
+		}
+		fmt.Println(n)
+	}
 }
